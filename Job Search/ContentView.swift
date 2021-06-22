6488ffff5ece3d1a2f72b1e9063e25b902f85ef7
Rocky Evans <rocky.evans@lovelandinnovations.com>
2021-06-22 11:19:39 -0600

    Initial Commit

---
+++ Job Search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Job Search
+//
+//  Created by Rocky Evans on 6/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Job Search/Job_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_SearchApp.swift
+//  Job Search
+//
+//  Created by Rocky Evans on 6/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Job_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
